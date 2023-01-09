c77ed5a9fcc5cf48530b8f6861b2ae92eb5573c9
Sam Johnston <johnstonsam712@gmail.com>
2023-01-09 15:09:12 +0000

    Disable other checkboxes for level when selected

--- app/components/NumberCheckbox.tsx
+++ app/components/NumberCheckbox.tsx
@@ -1,18 +1,53 @@
-import React, { useState } from "react";
+import React from "react"
 
 type NumberCheckboxProps = {
-  n: number;
-};
+  index: number
+  displayNumber?: boolean
+  skills: any
+  setSkills: React.Dispatch<any>
+  skillsKey: string
+  render: boolean
+  setRender: React.Dispatch<boolean>
+}
 
-export default function NumberCheckbox({ n }: NumberCheckboxProps) {
-  const [checked, setChecked] = useState(false);
+export default function NumberCheckbox({
+  index,
+  displayNumber = true,
+  setSkills,
+  skills,
+  skillsKey,
+  render,
+  setRender,
+}: NumberCheckboxProps) {
+  const skill = skills[skillsKey][index]
+  const checked = skill.toggled
+  const locked = skill.locked
   return (
     <button
-      onClick={() => setChecked(!checked)}
-      className={`flex justify-center items-center w-6 h-6 text-center align-middle rounded p-2 transition ${
-        checked ? "bg-indigo-500 text-white" : "bg-slate-400 text-slate-400"
+      onClick={() => {
+        const keys = Object.keys(skills).filter((key) => key !== skillsKey)
+        const clone = skills
+        const skillIndexVal = clone[skillsKey][index].toggled
+
+        keys.forEach((key) => {
+          clone[key][index] = {
+            toggled: false,
+            locked: !skillIndexVal,
+          }
+        })
+
+        clone[skillsKey][index] = { toggled: !skillIndexVal, locked: false }
+        setSkills(clone)
+        setRender(!render)
+      }}
+      className={`flex justify-center items-center w-6 h-6 text-center shadow-sm rounded p-4 transition  ${
+        checked
+          ? "bg-indigo-500 text-white"
+          : locked
+          ? "bg-slate-200 text-slate-200"
+          : "bg-slate-400 text-slate-400"
       }`}>
-      {n}
+      {displayNumber ? index + 1 : null}
     </button>
-  );
+  )
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,60 +1,69 @@
-import { useState } from "react";
-import NumberCheckbox from "~/components/NumberCheckbox";
+import React, { useState } from "react"
+import NumberCheckbox from "~/components/NumberCheckbox"
 
 export default function Index() {
-  const LEVELS = 18;
-  const [skills, setSkills] = useState<any>({
-    q: Array(LEVELS).fill(undefined),
-    e: Array(LEVELS).fill(undefined),
-    rightClick: Array(LEVELS).fill(undefined),
-    r: Array(LEVELS).fill(undefined)
-  });
+  const LEVELS = 18
+  const ogSkillToggle = {
+    toggled: false,
+    locked: false,
+  }
+  const originalSkills = {
+    q: Array(LEVELS).fill(ogSkillToggle),
+    e: Array(LEVELS).fill(ogSkillToggle),
+    rightClick: Array(LEVELS).fill(ogSkillToggle),
+    r: Array(LEVELS).fill(ogSkillToggle),
+  }
+  const [skills, setSkills] = useState<any>(originalSkills)
+  const [render, setRender] = useState(false)
+
   return (
     <>
-      <div className='h-screen flex flex-col justify-center items-center'>
-        <h1 className='text-5xl mb-8 font-bold tracking-wide self-start ml-64'>
+      <div className="flex w-fit m-auto flex-col justify-center items-center">
+        <h1 className="text-5xl mb-8 font-bold tracking-wide self-start">
           Skill Order
         </h1>
-        {/* <div className='flex'>
-        <div className='w-40'></div>
-        <div className='flex mb-2 gap-2'>
-          {Array(18)
-            .fill(undefined)
-            .map((a, index) => (
-              <span key={`skill-column-${index}`} className='w-4 text-center'>
-                {index + 1}
-              </span>
-            ))}
+        <div className="flex">
+          <div className="w-40"></div>
+          <div className="flex mb-2 gap-3 w-[780px]">
+            {Array(18)
+              .fill(undefined)
+              .map((a, index) => (
+                <span key={`skill-column-${index}`} className="w-8 text-center">
+                  {index + 1}
+                </span>
+              ))}
+          </div>
         </div>
-      </div> */}
         {Object.entries(skills).map(([key]) => (
-          <div className='flex mb-3' key={key}>
-            <span className='w-40'>{key}</span>
-            <div className='flex gap-3'>
+          <div className="flex mb-3" key={`skill-${key}`}>
+            <span className="w-40">{key}</span>
+            <div className="flex gap-3">
               {skills[key].map((skill, i) => (
-                <>
-                  <NumberCheckbox n={i + 1} />
-                  {/* <input
-                  type='checkbox'
-                  className='w-4'
-                  key={`${key}-${i}`}
-                  name={key}
-                  id={key}
-                /> */}
-                </>
+                <NumberCheckbox
+                  key={`skill-${key}-${i}`}
+                  skillsKey={key}
+                  skills={skills}
+                  setSkills={setSkills}
+                  index={i}
+                  render={render}
+                  setRender={setRender}
+                />
               ))}
             </div>
           </div>
         ))}
-        <div className='flex gap-2'>
-          <button className='p-2 rounded bg-indigo-500 text-white mt-2 disabled:opacity-30 transition'>
+        <div className="flex gap-2">
+          <button className="p-2 rounded bg-indigo-500 text-white mt-2 disabled:opacity-30 transition">
             Submit
           </button>
-          <button className='p-2 rounded bg-pink-500 text-white mt-2 disabled:opacity-30 transition'>
+          <button
+            className="p-2 rounded bg-pink-500 text-white mt-2 disabled:opacity-30 transition"
+            onClick={() => setSkills(originalSkills)}>
             Reset
           </button>
         </div>
       </div>
+      <pre className="w-1/2">{JSON.stringify(skills, null, 2)}</pre>
     </>
-  );
+  )
 }
