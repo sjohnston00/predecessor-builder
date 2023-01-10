22e53eea57c9dc2885fa516980c8b69089bdf0f1
Sam Jonston <johnstonsam712@gmail.com>
2023-01-10 21:53:25 +0000

    Make pred game like skill order

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -85,6 +85,10 @@ export default function Index() {
     console.log(levels);
   };
 
+  const smallSkillSize = `w-16 h-16`;
+  const skillPointSize = `w-3 h-3`;
+  const ultSkillSize = `w-32 h-32`;
+
   return (
     <>
       <div className='flex w-fit m-auto flex-col justify-center items-center'>
@@ -139,6 +143,98 @@ export default function Index() {
             onClick={() => setSkills(originalSkills)}>
             Reset
           </button>
+        </div>
+      </div>
+      <div className='flex justify-center my-24 gap-8 items-end'>
+        <div className='flex flex-col gap-8 justify-center items-center'>
+          <div
+            className={`${smallSkillSize} bg-slate-900 bg-opacity-70 rotate-45 flex justify-center items-center`}>
+            <span className='-rotate-45 text-lg text-white'>Q</span>
+          </div>
+          <div className='flex gap-2.5'>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-8 justify-center items-center'>
+          <div
+            className={`${smallSkillSize} bg-slate-900 bg-opacity-70 rotate-45 flex justify-center items-center`}>
+            <span className='-rotate-45 text-lg text-white'>E</span>
+          </div>
+          <div className='flex gap-2.5'>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-10 justify-center items-center'>
+          <div
+            className={`${ultSkillSize} bg-slate-900 bg-opacity-70 rotate-45 flex justify-center items-center`}>
+            <span className='-rotate-45 text-lg text-white'>R</span>
+          </div>
+          <div className='flex gap-2.5'>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-8 justify-center items-center'>
+          <div
+            className={`${smallSkillSize} bg-slate-900 bg-opacity-70 rotate-45 flex justify-center items-center`}>
+            <span className='-rotate-45 text-lg text-white'>L Clck</span>
+          </div>
+          <div className='flex gap-2.5'>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+          </div>
+        </div>
+        <div className='flex flex-col gap-8 justify-center items-center'>
+          <div
+            className={`${smallSkillSize} bg-slate-900 bg-opacity-70 rotate-45 flex justify-center items-center`}>
+            <span className='-rotate-45 text-lg text-white'>R Clck</span>
+          </div>
+          <div className='flex gap-2.5'>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+            <div
+              className={`${skillPointSize} bg-slate-900 bg-opacity-10 rotate-45 flex justify-center items-center`}></div>
+          </div>
         </div>
       </div>
       <pre className='w-1/2'>{JSON.stringify(skills, null, 2)}</pre>
