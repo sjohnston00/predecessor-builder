f53b52433952185702e66dd01a4864f9ad22aca9
Sam Johnston <johnstonsam712@gmail.com>
2023-01-16 13:44:39 +0000

    Add user to build model

---
+++ prisma/migrations/20230116134219_add_user_to_build/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Build" ADD COLUMN     "userId" INTEGER NOT NULL DEFAULT 1;
+
+-- AddForeignKey
+ALTER TABLE "Build" ADD CONSTRAINT "Build_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
