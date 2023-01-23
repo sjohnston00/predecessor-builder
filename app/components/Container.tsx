9f338cb82fbd6d1d38e38a1609bcdc59f2cd7e48
Sam Johnston <johnstonsam712@gmail.com>
2023-01-23 09:05:02 +0000

    Add logout button to navbar

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from "react"
 
 export default function Container({
   children,
+  className,
   ...props
 }: React.DetailedHTMLProps<
   React.HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >) {
   return (
-    <div className='px-1 max-w-2xl m-auto' {...props}>
+    <div className={`px-1 max-w-2xl m-auto ${className}`} {...props}>
       {children}
     </div>
-  );
+  )
 }
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,50 +1,59 @@
-import type { User } from "@prisma/client";
-import { Link } from "@remix-run/react";
-import React from "react";
+import React from "react"
+import type { User } from "@prisma/client"
+import { Form, Link } from "@remix-run/react"
+import Button from "./Button"
+import Container from "./Container"
 
 type Props = {
-  user: User | null;
-};
+  user: User | null
+}
 
 export default function Navbar({ user }: Props) {
   return (
-    <nav className='h-12 px-1 bg-indigo-500 text-white'>
-      <ul className='flex h-full gap-2 justify-between items-center'>
-        <div className='flex gap-2'>
-          <li>
-            <Link className='p-1' to={"/heroes"}>
-              Heroes
-            </Link>
-          </li>
-          <li>
-            <Link className='p-1' to={"/builds"}>
-              Builds
-            </Link>
-          </li>
-        </div>
-        {user ? (
-          <div className='flex gap-2'>
+    <nav className="h-12 bg-indigo-500 text-white">
+      <Container className="h-full">
+        <ul className="flex h-full gap-2 justify-between items-center">
+          <div className="flex items-center gap-2">
             <li>
-              <Link className='p-1' to={`/users/${user.username}`}>
-                My Account
+              <Link className="p-1" to={"/heroes"}>
+                Heroes
+              </Link>
+            </li>
+            <li>
+              <Link className="p-1" to={"/builds"}>
+                Builds
               </Link>
             </li>
           </div>
-        ) : (
-          <div className='flex gap-2'>
-            <li>
-              <Link className='p-1' to={`/register`}>
-                Register
-              </Link>
-            </li>
-            <li>
-              <Link className='p-1' to={`/login`}>
-                Login
-              </Link>
-            </li>
-          </div>
-        )}
-      </ul>
+          {user ? (
+            <div className="flex items-center gap-2">
+              <li>
+                <Link className="p-1" to={`/users/${user.username}`}>
+                  My Account
+                </Link>
+              </li>
+              <li>
+                <Form method="post" action="/logout">
+                  <Button type="submit">Logout</Button>
+                </Form>
+              </li>
+            </div>
+          ) : (
+            <div className="flex gap-2">
+              <li>
+                <Link className="p-1" to={`/register`}>
+                  Register
+                </Link>
+              </li>
+              <li>
+                <Link className="p-1" to={`/login`}>
+                  Login
+                </Link>
+              </li>
+            </div>
+          )}
+        </ul>
+      </Container>
     </nav>
-  );
+  )
 }
