5c69f2de6157132552d430e69f93c58ee02a81f1
Sam Johnston <johnstonsam712@gmail.com>
2023-01-24 10:25:13 +0000

    Fix button opacity on disabled prop

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React from "react"
 
 export default function Button({
+  children,
+  type,
+  className,
   ...props
 }: React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >) {
-  const { children, type, className } = props;
   return (
     <button
-      {...props}
       type={type || "button"}
-      className={`px-3 py-2 rounded bg-indigo-500 text-white disabled:opacity=-30 transition ${className}`}>
+      className={`px-3 py-2 rounded bg-indigo-500 text-white disabled:opacity-30 transition ${className}`}
+      {...props}>
       {children || "Button"}
     </button>
-  );
+  )
 }
--- app/routes/register.tsx
+++ app/routes/register.tsx
@@ -1,63 +1,63 @@
-import React from "react";
-import type { ActionArgs, LoaderArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useTransition } from "@remix-run/react";
-import Label from "~/components/Label";
-import { prisma } from "~/utils/prisma.server";
+import React from "react"
+import type { ActionArgs, LoaderArgs } from "@remix-run/node"
+import { redirect } from "@remix-run/node"
+import { Form, Link, useActionData, useTransition } from "@remix-run/react"
+import Label from "~/components/Label"
+import { prisma } from "~/utils/prisma.server"
 import {
   USERNAME_MAX_LENGTH,
   USERNAME_MIN_LENGTH,
   userSchema,
-  validatePassword
-} from "~/utils/user.server";
-import bcrypt from "bcryptjs";
-import Input from "~/components/Input";
-import Button from "~/components/Button";
-import PasswordInput from "~/components/PasswordInput";
-import { createUserSession, getUser } from "~/utils/session.server";
-import Container from "~/components/Container";
-import Heading from "~/components/Heading";
+  validatePassword,
+} from "~/utils/user.server"
+import bcrypt from "bcryptjs"
+import Input from "~/components/Input"
+import Button from "~/components/Button"
+import PasswordInput from "~/components/PasswordInput"
+import { createUserSession, getUser } from "~/utils/session.server"
+import Container from "~/components/Container"
+import Heading from "~/components/Heading"
 
 export const loader = async ({ request }: LoaderArgs) => {
-  const loggedInUser = await getUser(request);
+  const loggedInUser = await getUser(request)
   if (loggedInUser) {
-    return redirect(`/users/${loggedInUser.username}`);
+    return redirect(`/users/${loggedInUser.username}`)
   }
-  return null;
-};
+  return null
+}
 
 export const action = async ({ request }: ActionArgs) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
 
-  const username = data.username.toString();
-  const password = data.password.toString();
+  const username = data.username.toString()
+  const newPassword = data["new-password"].toString()
 
-  const validPassword = validatePassword(password);
+  const validPassword = validatePassword(newPassword)
 
   userSchema.parse({
     username,
-    password
-  });
+    password: newPassword,
+  })
 
   if (!validPassword) {
     return {
-      message: `Password must contain: Lowercase, Uppercase, Special Characters and a Number`
-    };
+      message: `Password must contain: Lowercase, Uppercase, Special Characters and a Number`,
+    }
   }
 
-  const passwordHash = await bcrypt.hash(password, 10);
+  const passwordHash = await bcrypt.hash(newPassword, 10)
 
   const userExists = await prisma.user.findFirst({
     where: {
-      username
-    }
-  });
+      username,
+    },
+  })
 
   if (userExists) {
     return {
-      message: `User with this email already exists`
-    };
+      message: `User with this email already exists`,
+    }
   }
 
   const { userId } = await prisma.user.create({
@@ -65,63 +65,67 @@ export const action = async ({ request }: ActionArgs) => {
       username: username,
       role: {
         connect: {
-          name: "User"
-        }
-      }
-    }
-  });
+          name: "User",
+        },
+      },
+    },
+  })
 
   await prisma.userPassword.create({
     data: {
       passwordHash,
-      userId
-    }
-  });
+      userId,
+    },
+  })
 
-  return createUserSession(userId, `/users/${username}`);
-};
+  return createUserSession(userId, `/users/${username}`)
+}
 
 export default function Register() {
-  const { state } = useTransition();
-  const actionData = useActionData();
+  const { state } = useTransition()
+  const actionData = useActionData()
 
-  const isSubmitting = state === "submitting";
+  const isSubmitting = state === "submitting"
   return (
     <Container>
-      <Form method='post' className='flex flex-col justify-center h-screen'>
-        <Heading type='h1' className='mb-2'>
+      <Form method="post" className="flex flex-col justify-center h-screen">
+        <Heading type="h1" className="mb-2">
           Register
         </Heading>
         {actionData?.message ? (
-          <div className='text-sm px-2 py-3 bg-red-500 rounded text-white mb-2'>
+          <div className="text-sm px-2 py-3 bg-red-500 rounded text-white mb-2">
             {actionData?.message}
           </div>
         ) : null}
-        <Label htmlFor='username' required>
+        <Label htmlFor="username" required>
           Username
         </Label>
         <Input
-          type='username'
-          name='username'
-          id='username'
+          type="username"
+          name="username"
+          id="username"
           minLength={USERNAME_MIN_LENGTH}
           maxLength={USERNAME_MAX_LENGTH}
           required
         />
-        <Label htmlFor='password' required>
+        <Label htmlFor="password" required>
           New Password
         </Label>
-        <PasswordInput autoComplete='new-password' />
+        <PasswordInput
+          autoComplete="new-password"
+          name="new-password"
+          id="new-password"
+        />
 
-        <div className='flex items-baseline gap-2'>
-          <Button type='submit' disabled={isSubmitting}>
+        <div className="flex items-baseline gap-2">
+          <Button type="submit" disabled={isSubmitting}>
             Register
           </Button>
-          <Link to={"/login"} className='text-indigo-500 underline p-1'>
+          <Link to={"/login"} className="text-indigo-500 underline p-1">
             Already a member? Login
           </Link>
         </div>
       </Form>
     </Container>
-  );
+  )
 }
