dfdf0ac071d414b1e18345c1f423ed5f8ec46d00
Sam Johnston <johnstonsam712@gmail.com>
2023-01-24 11:07:50 +0000

    add role to build/new

--- app/routes/builds/new.tsx
+++ app/routes/builds/new.tsx
@@ -1,158 +1,174 @@
-import React, { useState, useRef } from "react";
-import type { ActionArgs } from "@remix-run/node";
-import { redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
-import { prisma } from "~/utils/prisma.server";
-import useKeypress from "react-use-keypress";
+import React, { useState, useRef } from "react"
+import type { ActionArgs } from "@remix-run/node"
+import { redirect } from "@remix-run/node"
+import { Form, useLoaderData } from "@remix-run/react"
+import { prisma } from "~/utils/prisma.server"
+import useKeypress from "react-use-keypress"
 import {
   checkAbilityMaxed,
   getAllOtherKeys,
-  lockOtherSkillsForIndex
-} from "~/utils";
-import NumberCheckbox from "~/components/NumberCheckbox";
-import { getUserId } from "~/utils/session.server";
-import Input from "~/components/Input";
-import Select from "~/components/Select";
-import Heading from "~/components/Heading";
+  lockOtherSkillsForIndex,
+} from "~/utils"
+import NumberCheckbox from "~/components/NumberCheckbox"
+import { getUserId } from "~/utils/session.server"
+import Input from "~/components/Input"
+import Select from "~/components/Select"
+import Heading from "~/components/Heading"
+import Label from "~/components/Label"
 
 export const loader = async () => {
-  const heroes = await prisma.hero.findMany();
+  const heroes = await prisma.hero.findMany()
   return {
-    heroes
-  };
-};
+    heroes,
+  }
+}
 
 export const action = async ({ request }: ActionArgs) => {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  const levels = JSON.parse(data.skillOrder.toString()) as string[];
-  const heroName = data.hero.toString();
-  const buildName = data.name.toString();
-  const loggedInUserId = await getUserId(request);
+  const formData = await request.formData()
+  const data = Object.fromEntries(formData)
+  const levels = JSON.parse(data.skillOrder.toString()) as string[]
+  const heroName = data.hero.toString()
+  const buildRole = data.role.toString()
+  const buildName = data.name.toString().replaceAll(" ", "-")
+  const loggedInUserId = await getUserId(request)
   const hero = await prisma.hero.findFirst({
     where: {
       name: {
         equals: heroName,
-        mode: "insensitive"
-      }
-    }
-  });
+        mode: "insensitive",
+      },
+    },
+  })
 
   if (!hero) {
     return {
-      message: `Couldn't find a hero with name "${heroName}"`
-    };
+      message: `Couldn't find a hero with name "${heroName}"`,
+    }
   }
 
   const newBuild = await prisma.build.create({
     data: {
       name: buildName || "No Name",
       abilityOrder: levels,
+      role: buildRole || "Any",
       heroId: hero.heroId,
-      userId: loggedInUserId
-    }
-  });
+      userId: loggedInUserId,
+    },
+  })
 
-  throw redirect(`/builds/${heroName}/${newBuild.name}`);
-};
+  throw redirect(`/builds/${heroName}/${newBuild.name}`)
+}
 
 export default function NewBuild() {
-  const buildNameRef = useRef<HTMLInputElement>(null);
-  const { heroes } = useLoaderData<typeof loader>();
+  const buildNameRef = useRef<HTMLInputElement>(null)
+  const buildRoleRef = useRef<HTMLInputElement>(null)
+  const { heroes } = useLoaderData<typeof loader>()
   useKeypress(["q", "e", "r", "t", "Backspace"], ({ key }) => {
-    if (document.activeElement === buildNameRef.current) return;
+    if (
+      document.activeElement?.id === buildNameRef.current?.id ||
+      document.activeElement?.id === buildRoleRef.current?.id
+    )
+      return null
 
-    let ability = key;
+    let ability = key
     if (key === "t") {
-      ability = "rightClick";
+      ability = "rightClick"
     }
 
     if (key === "Backspace") {
-      console.log("Set the last instance of true to ogSkillToggle");
-      return;
+      console.log("Set the last instance of true to ogSkillToggle")
+      return
     }
 
-    const lastIndex = lastToggledIndex();
-    const nextQIndex = lastIndex === -1 ? 0 : lastIndex + 1; //if none is toggle this will return -1 so we start with index 0, else we want to go to the next line
-    if (nextQIndex >= LEVELS) return;
+    const lastIndex = lastToggledIndex()
+    const nextQIndex = lastIndex === -1 ? 0 : lastIndex + 1 //if none is toggle this will return -1 so we start with index 0, else we want to go to the next line
+    if (nextQIndex >= LEVELS) return
 
-    const clone = skills;
+    const clone = skills
 
-    const keys = getAllOtherKeys(ability, clone);
-    lockOtherSkillsForIndex(keys, clone, nextQIndex);
+    const keys = getAllOtherKeys(ability, clone)
+    lockOtherSkillsForIndex(keys, clone, nextQIndex)
 
-    const abilityCount = levels.filter((level) => level === ability).length;
+    const abilityCount = levels.filter((level) => level === ability).length
 
     if (checkAbilityMaxed(ability, abilityCount)) {
-      alert(`ability: "${ability}" has already been maxed out`);
-      return;
+      alert(`ability: "${ability}" has already been maxed out`)
+      return
     }
 
     clone[ability][nextQIndex] = {
       toggled: true,
-      locked: false
-    };
-    setSkills(clone);
-    setRender(!render);
-  });
-  const LEVELS = 18;
+      locked: false,
+    }
+    setSkills(clone)
+    setRender(!render)
+  })
+  const LEVELS = 18
   const ogSkillToggle = {
     toggled: false,
-    locked: false
-  };
+    locked: false,
+  }
   const originalSkills = {
     q: Array(LEVELS).fill(ogSkillToggle),
     e: Array(LEVELS).fill(ogSkillToggle),
     rightClick: Array(LEVELS).fill(ogSkillToggle),
-    r: Array(LEVELS).fill(ogSkillToggle)
-  };
-  const [skills, setSkills] = useState<any>(originalSkills);
-  const [render, setRender] = useState(false);
+    r: Array(LEVELS).fill(ogSkillToggle),
+  }
+  const [skills, setSkills] = useState<any>(originalSkills)
+  const [render, setRender] = useState(false)
 
-  const levels = Array(LEVELS).fill(undefined);
+  const levels = Array(LEVELS).fill(undefined)
   Object.entries(skills).forEach(([key]) => {
     skills[key].forEach((skillLevel, index) =>
       skillLevel.toggled ? (levels[index] = key) : null
-    );
-  });
-  const levelsIncomplete = levels.includes(undefined);
+    )
+  })
+  const levelsIncomplete = levels.includes(undefined)
 
   const lastToggledIndex = () => {
-    let index = -1;
+    let index = -1
     Object.entries(skills).forEach(([key]) => {
-      const abilities = skills[key].map((skill) => skill.toggled);
-      const lastIndex = abilities.lastIndexOf(true);
+      const abilities = skills[key].map((skill) => skill.toggled)
+      const lastIndex = abilities.lastIndexOf(true)
       if (lastIndex > index) {
-        index = lastIndex;
+        index = lastIndex
       }
-    });
-    return index;
-  };
+    })
+    return index
+  }
 
   return (
     <>
-      <div className='flex w-fit m-auto flex-col justify-center items-center'>
-        <Heading type='h1' className='mb-4 self-start'>
+      <div className="flex w-fit m-auto flex-col justify-center items-center">
+        <Heading type="h1" className="mb-4 self-start">
           New Build
         </Heading>
-        <Form method='post'>
-          <div className='self-start mb-4'>
-            <label htmlFor='name' className='block'>
-              Build Name
-            </label>
+        <Form method="post">
+          <div className="self-start mb-4">
+            <Label htmlFor="name">Build Name</Label>
             <Input
-              type='text'
-              name='name'
-              id='name'
+              type="text"
+              name="name"
+              id="name"
               ref={buildNameRef}
+              minLength={3}
+              maxLength={30}
+              required
+            />
+            <Label htmlFor="role">Lane / Role</Label>
+            <Input
+              type="text"
+              name="role"
+              id="role"
+              ref={buildRoleRef}
               required
             />
           </div>
-          <div className='self-start mb-4'>
-            <label htmlFor='hero' className='block'>
+          <div className="self-start mb-4">
+            <label htmlFor="hero" className="block">
               Hero
             </label>
-            <Select name='hero' id='hero' required>
+            <Select name="hero" id="hero" required>
               {heroes.map((hero) => (
                 <option key={hero.name} value={hero.name}>
                   {hero.name}
@@ -160,33 +176,33 @@ export default function NewBuild() {
               ))}
             </Select>
           </div>
-          <h2 className='text-3xl mb-4 font-bold tracking-wide self-start'>
+          <h2 className="text-3xl mb-4 font-bold tracking-wide self-start">
             Skill Order
-            <span className='block text-sm font-normal opacity-50 mt-4'>
+            <span className="block text-sm font-normal opacity-50 mt-4">
               (Pressing the skill key will add to the skill order and backspace
               will remove the last one (try pressing 'q', then 'backspace'))
             </span>
           </h2>
-          <div className='flex'>
-            <div className='w-40'></div>
-            <div className='flex mb-2 gap-3'>
+          <div className="flex">
+            <div className="w-40"></div>
+            <div className="flex mb-2 gap-3">
               {Array(LEVELS)
                 .fill(undefined)
                 .map((a, index) => (
                   <span
                     key={`skill-column-${index}`}
-                    className='h-8 w-8 text-center rounded'>
+                    className="h-8 w-8 text-center rounded">
                     {index + 1}
                   </span>
                 ))}
             </div>
           </div>
           {Object.entries(skills).map(([key]) => (
-            <div className='flex mb-3' key={`skill-${key}`}>
-              <span className='w-40'>
+            <div className="flex mb-3" key={`skill-${key}`}>
+              <span className="w-40">
                 {key === "rightClick" ? `${key} (t)` : key}
               </span>
-              <div className='flex gap-3'>
+              <div className="flex gap-3">
                 {skills[key].map((skill, i) => (
                   <NumberCheckbox
                     key={`skill-${key}-${i}`}
@@ -201,29 +217,29 @@ export default function NewBuild() {
               </div>
             </div>
           ))}
-          <div className='flex gap-2'>
+          <div className="flex gap-2">
             <input
-              type='hidden'
-              name='skillOrder'
-              id='skillOrder'
+              type="hidden"
+              name="skillOrder"
+              id="skillOrder"
               value={JSON.stringify(levels)}
             />
             <button
-              type='submit'
-              className='p-2 rounded bg-indigo-500 text-white mt-2 disabled:opacity-30 transition'
+              type="submit"
+              className="p-2 rounded bg-indigo-500 text-white mt-2 disabled:opacity-30 transition"
               // onClick={calcLevels}
               disabled={levelsIncomplete}>
               Submit
             </button>
             <button
-              className='p-2 rounded bg-pink-500 text-white mt-2 disabled:opacity-30 transition'
+              className="p-2 rounded bg-pink-500 text-white mt-2 disabled:opacity-30 transition"
               onClick={() => setSkills(originalSkills)}
-              type='button'>
+              type="button">
               Reset
             </button>
           </div>
         </Form>
       </div>
     </>
-  );
+  )
 }
