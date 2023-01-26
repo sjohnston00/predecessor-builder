2b421464874b4e2dc2e5719ed7c266a6569f2ff7
Sam Johnston <johnstonsam712@gmail.com>
2023-01-26 14:15:16 +0000

    fix .env variables

--- env.ts
+++ env.ts
@@ -1,11 +1,13 @@
-import { z } from "zod";
+import { z } from "zod"
 
 const envVariables = z.object({
   DATABASE_URL: z.string(),
-  SESSION_SECRET: z.string()
-});
+  SESSION_SECRET: z.string(),
+  CLERK_PUBLISHABLE_KEY: z.string(),
+  CLERK_SECRET_KEY: z.string(),
+})
 
-envVariables.parse(process.env);
+envVariables.parse(process.env)
 
 declare global {
   namespace NodeJS {
