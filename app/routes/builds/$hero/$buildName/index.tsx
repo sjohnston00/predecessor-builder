2a6f75d7429be608eef252e435929b3b81ecc643
Sam Jonston <johnstonsam712@gmail.com>
2023-01-29 18:25:56 +0000

    change like button color

--- app/routes/builds/$hero/$buildName/index.tsx
+++ app/routes/builds/$hero/$buildName/index.tsx
@@ -43,11 +43,21 @@ export const loader = async (args: LoaderArgs) => {
       status: 404
     });
 
+  const allUsers = await clerkClient.users.getUserList();
   let buildUser: User | null | undefined;
 
   if (build.userId) {
-    buildUser = await clerkClient.users.getUser(build.userId);
+    buildUser = allUsers.find((user) => user.id === build.userId);
   }
+
+  const buildComments = build.comments.map((comment) => {
+    const user = allUsers.find((user) => user.id === comment.userId);
+
+    return {
+      ...comment,
+      user
+    };
+  });
 
   let buildItems: Item[] | undefined;
 
@@ -72,7 +82,8 @@ export const loader = async (args: LoaderArgs) => {
     build: {
       ...build,
       user: buildUser,
-      items: buildItems
+      items: buildItems,
+      comments: buildComments
     },
     hero,
     isUserLoggedIn: !!userId
@@ -176,12 +187,16 @@ export default function BuildNumber() {
         Comments <sup className='text-gray-400'>{build.comments.length}</sup>
       </Heading>
       {build.comments.map((comment) => (
-        <p key={comment.commentId}>
-          {comment.content} - {comment.userId}
+        <p className='mb-4 flex justify-between' key={comment.commentId}>
+          {comment.content}{" "}
+          <span className='text-gray-400'>
+            {comment.user?.username || "Anonymous"}
+          </span>
         </p>
       ))}
 
-      <p>Skill Order: [{build.abilityOrder.toLocaleString()}]</p>
+      <Heading type='h2'>Skill Order</Heading>
+      <p className='mb-4'>[{build.abilityOrder.toLocaleString()}]</p>
       {isLoggedInUsersBuild ? (
         <LinkButton to={`/builds/${build.hero.name}/${build.name}/edit`}>
           Edit
@@ -210,7 +225,7 @@ export default function BuildNumber() {
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 24 24'
               fill='currentColor'
-              className='w-6 h-6'>
+              className='w-6 h-6 text-indigo-500'>
               <path d='M7.493 18.75c-.425 0-.82-.236-.975-.632A7.48 7.48 0 016 15.375c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75 2.25 2.25 0 012.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23h-.777zM2.331 10.977a11.969 11.969 0 00-.831 4.398 12 12 0 00.52 3.507c.26.85 1.084 1.368 1.973 1.368H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 01-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227z' />
             </svg>
           ) : (
@@ -220,7 +235,7 @@ export default function BuildNumber() {
               viewBox='0 0 24 24'
               strokeWidth={1.5}
               stroke='currentColor'
-              className='w-6 h-6'>
+              className='w-6 h-6 text-indigo-500'>
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
@@ -228,7 +243,7 @@ export default function BuildNumber() {
               />
             </svg>
           )}
-          <span className='text-gray-600'>{build.likes.length}</span>
+          <small className='text-indigo-300'>{build.likes.length}</small>
         </Button>
       </Form>
       <Form method='post'>
